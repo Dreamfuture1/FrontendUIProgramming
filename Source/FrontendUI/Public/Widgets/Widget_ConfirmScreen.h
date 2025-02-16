ea7fb9fb9420ad68ce05142bcfebb13a1173112d
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2025-02-16 17:01:16 +0800

    InitConfirmScreenDone

--- Source/FrontendUI/Public/Widgets/Widget_ConfirmScreen.h
+++ Source/FrontendUI/Public/Widgets/Widget_ConfirmScreen.h
@@ -50,6 +50,10 @@ class FRONTENDUI_API UWidget_ConfirmScreen : public UWidget_ActivatableBase
 {
 	GENERATED_BODY()
 	
+public:
+	//Gets called outside of the class when this widget is contructed and before it's pushed to the modal stack
+	void InitConfirmScreen(UConfirmScreenInfoObject* InScreenInfoObject,TFunction<void(EConfirmScreenButtonType)> ClickedButtonCallback);
+
 private:
 	UPROPERTY(meta = (BindWidget))
 	UCommonTextBlock* CommonTextBlock_Title;
