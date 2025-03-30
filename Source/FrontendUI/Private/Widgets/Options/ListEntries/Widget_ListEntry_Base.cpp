7f3919fc24d1e570207d71c9cd82cab527b94972
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2025-03-30 16:16:27 +0800

    ListEntryWidgetDone

---
+++ Source/FrontendUI/Private/Widgets/Options/ListEntries/Widget_ListEntry_Base.cpp
@@ -0,0 +1,9 @@
+// Vince Petrelli All Rights Reserved
+
+
+#include "Widgets/Options/ListEntries/Widget_ListEntry_Base.h"
+
+void UWidget_ListEntry_Base::NativeOnListItemObjectSet(UObject* ListItemObject)
+{
+	IUserObjectListEntry::NativeOnListItemObjectSet(ListItemObject);
+}
