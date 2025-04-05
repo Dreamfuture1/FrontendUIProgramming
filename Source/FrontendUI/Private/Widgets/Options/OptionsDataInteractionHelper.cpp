0d7b2c42ca68feee36fc53a6ebe27f9f1adefa73
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2025-04-05 17:03:00 +0800

    InteractionHelperDone

---
+++ Source/FrontendUI/Private/Widgets/Options/OptionsDataInteractionHelper.cpp
@@ -0,0 +1,32 @@
+// Vince Petrelli All Rights Reserved
+
+
+#include "Widgets/Options/OptionsDataInteractionHelper.h"
+#include "FontendSettings/FrontendGameUserSettings.h"
+
+FOptionsDataInteractionHelper::FOptionsDataInteractionHelper(const FString& InSetterOrGetterFuncPath)
+	: CachedDynamicFunctionPath(InSetterOrGetterFuncPath)
+{
+	CachedWeakGameUserSettings = UFrontendGameUserSettings::Get();
+}
+
+FString FOptionsDataInteractionHelper::GetValueAsString() const
+{	
+	FString OutStringValue;
+	PropertyPathHelpers::GetPropertyValueAsString(
+		CachedWeakGameUserSettings.Get(),
+		CachedDynamicFunctionPath,
+		OutStringValue
+	);
+
+	return OutStringValue;
+}
+
+void FOptionsDataInteractionHelper::SetValueFromString(const FString& InStringValue)
+{
+	PropertyPathHelpers::SetPropertyValueFromString(
+		CachedWeakGameUserSettings.Get(),
+		CachedDynamicFunctionPath,
+		InStringValue
+	);
+}
