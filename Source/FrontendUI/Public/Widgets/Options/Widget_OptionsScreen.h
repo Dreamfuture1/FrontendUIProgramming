253e41a2346ff4805538ccdb64bb92fb300b8a26
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2025-04-13 21:00:50 +0800

    RegainFocusForGamepad

--- Source/FrontendUI/Public/Widgets/Options/Widget_OptionsScreen.h
+++ Source/FrontendUI/Public/Widgets/Options/Widget_OptionsScreen.h
@@ -28,6 +28,7 @@ protected:
 	//~ Begin UCommonActivatableWidget Interface
 	virtual void NativeOnActivated() override;
 	virtual void NativeOnDeactivated() override; 
+	virtual UWidget* NativeGetDesiredFocusTarget() const override;
 	//~ End UCommonActivatableWidget Interface
 
 private:
