09bc6f311ccb756e64b5c8fd700fbd424a84ef34
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2025-04-13 22:10:29 +0800

    DisplayDescriptionImageDone

--- Source/FrontendUI/Private/FrontendFunctionLibrary.cpp
+++ Source/FrontendUI/Private/FrontendFunctionLibrary.cpp
@@ -12,3 +12,13 @@ TSoftClassPtr<UWidget_ActivatableBase> UFrontendFunctionLibrary::GetFrontendSoft
 
 	return FrontendDeveloperSettings->FrontendWidgetMap.FindRef(InWidgetTag);
 }
+
+TSoftObjectPtr<UTexture2D> UFrontendFunctionLibrary::GetOptionsSoftImageByTag(FGameplayTag InImageTag)
+{
+	const UFrontendDeveloperSettings* FrontendDeveloperSettings = GetDefault<UFrontendDeveloperSettings>();
+
+	checkf(FrontendDeveloperSettings->OptionsScreenSoftImageMap.Contains(InImageTag),TEXT("Could not find an image accociated with tag %s"),*InImageTag.ToString());
+
+	return FrontendDeveloperSettings->OptionsScreenSoftImageMap.FindRef(InImageTag);
+
+}
