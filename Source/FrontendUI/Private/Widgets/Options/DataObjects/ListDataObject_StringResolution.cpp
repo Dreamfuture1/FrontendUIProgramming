70ab1d996d4662fc42065fd4cd8a84641004c4c0
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2025-05-08 15:57:41 +0800

    InitResolutionValuesDone

--- Source/FrontendUI/Private/Widgets/Options/DataObjects/ListDataObject_StringResolution.cpp
+++ Source/FrontendUI/Private/Widgets/Options/DataObjects/ListDataObject_StringResolution.cpp
@@ -2,7 +2,30 @@
 
 
 #include "Widgets/Options/DataObjects/ListDataObject_StringResolution.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Widgets/Options/OptionsDataInteractionHelper.h"
+
+#include "FrontendDebugHelper.h"
 
 void UListDataObject_StringResolution::InitResolutionValues()
+{	
+	TArray<FIntPoint> AvaialbleResolutions;
+
+	UKismetSystemLibrary::GetSupportedFullscreenResolutions(AvaialbleResolutions);
+
+	for (const FIntPoint& Resolution : AvaialbleResolutions)
+	{
+		Debug::Print(TEXT("Avaialble Resolution: ") + Resolution.ToString());
+	}
+}
+
+void UListDataObject_StringResolution::OnDataObjectInitialized()
 {
+	Super::OnDataObjectInitialized();
+
+	if (DataDynamicGetter)
+	{	
+		Debug::Print(TEXT("Resolution Value from Dynamic Getter: ") + DataDynamicGetter->GetValueAsString());
+		
+	}
 }
