ccad8138aaa5ef8524e4f5987e5d656107596b96
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2025-05-08 19:10:38 +0800

    OnToggleEditableStateDone

--- Source/FrontendUI/Private/Widgets/Options/ListEntries/Widget_ListEntry_Base.cpp
+++ Source/FrontendUI/Private/Widgets/Options/ListEntries/Widget_ListEntry_Base.cpp
@@ -55,10 +55,20 @@ void UWidget_ListEntry_Base::OnOwningListDataObjectSet(UListDataObject_Base* InO
 	{
 		InOwningListDataObject->OnListDataModified.AddUObject(this,&ThisClass::OnOwningListDataObjectModified);
 	}
+
+	OnToggleEditableState(InOwningListDataObject->IsDataCurrentlyEditable());
 }
 
 void UWidget_ListEntry_Base::OnOwningListDataObjectModified(UListDataObject_Base* OwningModifiedData, EOptionsListDataModifyReason ModifyReason)
 {
+}
+
+void UWidget_ListEntry_Base::OnToggleEditableState(bool bIsEditable)
+{
+	if (CommonText_SettingDisplayName)
+	{
+		CommonText_SettingDisplayName->SetIsEnabled(bIsEditable);
+	}
 }
 
 void UWidget_ListEntry_Base::SelectThisEntryWidget()
