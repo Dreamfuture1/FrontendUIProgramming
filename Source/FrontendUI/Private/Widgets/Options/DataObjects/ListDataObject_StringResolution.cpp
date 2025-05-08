58f7dac526aefcf4fc699076fc43cca6df9595b5
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2025-05-08 15:33:47 +0800

    ResolutionDataObjectDone

---
+++ Source/FrontendUI/Private/Widgets/Options/DataObjects/ListDataObject_StringResolution.cpp
@@ -0,0 +1,8 @@
+// Vince Petrelli All Rights Reserved
+
+
+#include "Widgets/Options/DataObjects/ListDataObject_StringResolution.h"
+
+void UListDataObject_StringResolution::InitResolutionValues()
+{
+}
