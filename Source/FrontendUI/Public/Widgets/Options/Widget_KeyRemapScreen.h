12de4a3faa21acf8a9eea79086a44243b446512b
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2025-05-22 15:20:34 +0800

    RequestDeactivateWidgetDone

--- Source/FrontendUI/Public/Widgets/Options/Widget_KeyRemapScreen.h
+++ Source/FrontendUI/Public/Widgets/Options/Widget_KeyRemapScreen.h
@@ -20,6 +20,12 @@ class FRONTENDUI_API UWidget_KeyRemapScreen : public UWidget_ActivatableBase
 public:
 	void SetDesiredInputTypeToFilter(ECommonInputType InDesiredInputType);
 
+	DECLARE_DELEGATE_OneParam(FOnKeyRemapScreenKeyPressedDelegate,const FKey& /*PressedKey*/)
+	FOnKeyRemapScreenKeyPressedDelegate OnKeyRemapScreenKeyPressed;
+
+	DECLARE_DELEGATE_OneParam(FOnKeyRemapScreenKeySelectCanceledDelegate,const FString& /*CanceledReason*/)
+	FOnKeyRemapScreenKeySelectCanceledDelegate OnKeyRemapScreenKeySelectCanceled;
+
 protected:
 	//~ Begin UCommonActivatableWidget Interface
 	virtual void NativeOnActivated() override;
@@ -29,6 +35,9 @@ protected:
 private:
 	void OnValidKeyPressedDetected(const FKey& PressedKey);
 	void OnKeySelectCanceled(const FString& CanceledReason);
+
+	//Delay a tick to make sure the input key is captured properly before calling the PreDeactivateCallback and deactivating the widget
+	void RequestDeactivateWidget(TFunction<void()> PreDeactivateCallback);
 
 	//***** Bound Widgets ***** //
 	UPROPERTY(meta = (BindWidget))
